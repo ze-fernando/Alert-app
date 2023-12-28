38b40ff6607cd89ea40c6410838a9cfec25c153b
ze-fernando <jfenginner935@gmail.com>
2023-12-27 21:15:34 -0300

    feat: signin

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
+
 
 class User(models.Model):
     name = models.CharField(max_length=255)
@@ -10,6 +12,7 @@ class User(models.Model):
     def save(self, *args, **kwargs):
         self.password = make_password(self.password)
         super().save(*args, **kwargs)
+    
 
 
 class Task(models.Model):
--- app/views.py
+++ app/views.py
@@ -4,23 +4,34 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
-
-
 @csrf_exempt
 @require_POST
 def signup(req):
     data = json.loads(req.body)
+    usr = User.objects.filter(name=data['name']).exists()
+    if usr:
+        return JsonResponse({'message': f'User {data['name']} aleredy exists'}, status=422)
     try:
         user = User(
             name = data['name'],
-            password = data['pass'],
+            password = data['password'],
             email = data['email'],
             tel = data['tel']
         )
         user.save()
-        return JsonResponse({'message': f"User {data['name']} created!"})
+        return JsonResponse({'message': f"User {data['name']} created!"}, status=201)
     
     except Exception as e:
-        return JsonResponse({'message': 'Internal server error', 'Error': f'{e}'})
+        return JsonResponse({'message': 'Internal server error', 'Error': f'{e}'}, status=500)
         
-        
+
+@csrf_exempt
+@require_POST
+def signin(req):
+    data = json.loads(req.body)
+    usr = User.objects.get(name=data['name'])
+    passw = check_password(data['password'], usr.password)
+    if passw:
+        return JsonResponse({'msg': 'true'}, status=200)
+    else:
+        return JsonResponse({'msg': 'false'}, status=401)
